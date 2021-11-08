40db648ee334da766cc995485721ea501275407f
Agisnaafn <agisnaa696@gmail.com>
2021-11-08 08:37:10 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya Nur Afni Agisna
+            Saya tinggal di daerah Ciamis
+            olah raga yang saya sukai adalah
+            Skipping";
+    }
+}
